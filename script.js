97b43ca89d8a9b4cc82f8a05233b0f65d4a819cc
joaquin <jecorrea@protonmail.com>
2022-01-29 11:49:11 -0500

    basic functionality

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ const slideLeft = document.querySelector(".left-slide");
 const upButton = document.querySelector(".up-button");
 const downButton = document.querySelector(".down-button");
 const slidesLength = slideRight.querySelectorAll("div").length;
+
+// console.log(slidesLength); # Gives you 4
+
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+upButton.addEventListener("click", () => {
+  changeSlide("up");
+});
+
+downButton.addEventListener("click", () => {
+  changeSlide("down");
+});
+
+const changeSlide = (direction) => {
+  sliderHeight = sliderContainer.clientHeight;
+  //   console.log(sliderHeight);
+};
